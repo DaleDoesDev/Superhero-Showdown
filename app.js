2bf1704773ca6a154064d1ed31eb607d0c706aef
DaleDoesDev <bumgardner.dale@gmail.com>
2021-10-04 14:38:34 -0400

    moved index.html into views

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-let express = require('express');
+let express = require("express");
 app = express();
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.use('*', (req, res) => {
-    res.redirect('/');
-})
+app.use("*", (req, res) => {
+  res.sendFile("index.html", {
+    root: "public/app/views/",
+  });
+});
 
 let PORT = process.env.PORT || 3000;
 app.listen(PORT);
-console.log(`App running on port: ${PORT}.`)
+console.log(`App running on port: ${PORT}.`);
